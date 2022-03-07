e420aca072bdf2824a3c5cebeb52c7385d74f5a3
Supasin Saetang <supasin.s1t@gmail.com>
2022-03-07 17:42:50 +0700

    website landing page success

---
+++ app.js
@@ -0,0 +1,7 @@
+const email = document.querySelector('#newsletter');
+const btnSubscribe = document.querySelector('.btn-subscribe');
+const formSubscribe = document.querySelector('.newsletter-form');
+
+formSubscribe.addEventListener('submit', e => {
+    e.preventDefault();
+})
